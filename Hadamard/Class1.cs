ac3c3dc2cf17a14e349a75386b1512a25bf95917
Peter Clayton <peter@bartendersacademy.com>
2021-09-26 21:21:27 -0400

    Add project files.

---
+++ Hadamard/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Hadamard
+{
+	public class Hadamard
+	{
+		public int Size { get; set;}
+		public List<List<int>> HadamardMatrix { get; set; }
+		ArrayList hMatrixRows { get; set; }
+
+		public Hadamard(int size = 2)
+        {
+			Size = size;
+			HadamardMatrix = generateHMatrix(Size);
+		}
+
+		private List<List<int>> generateHMatrix(int n)
+		{
+			List<List<int>> hMatrix = new List<List<int>>();
+			for (int i = 0; i < n; i++) {
+				hMatrix.Add(new List<int>());
+				for (int j = 0; j < n; j++)
+				{
+					hMatrix[i].Add(0);
+				}
+			}
+			
+			hMatrix[0][0] = 1;
+			for (int i1 = 1; i1 < n; i1+=i1) {
+				for (int i2 = 0; i2 < i1; i2++){
+					for (int i3 = 0; i3 < i1; i3++)
+					{
+						hMatrix[i2 + i1][i3] = hMatrix[i2][i3];
+						hMatrix[i2][i3 + i1] = hMatrix[i2][i3];
+						hMatrix[i2 + i1][i3 + i1] = -hMatrix[i2][i3];
+					}
+				}
+			}
+			return hMatrix;
+        }
+		private void printHadamardMatrix(List<List<int>> hadamardMatrix)
+		{
+			Console.WriteLine($"Inside the printer: Size: {hadamardMatrix.Count}");
+			for (int i=0;i< hadamardMatrix.Count; i++)
+			{
+				for (int j=0; j< hadamardMatrix[i].Count; j++)
+				{
+					Console.Write(hadamardMatrix[i][j] + "\t");
+				}
+				Console.WriteLine();
+			}
+		}
+
+		public void Run(List<List<int>> hadamardMatrix)
+		{
+			Console.WriteLine($"You've selected a Hadamard Matrix size of: {Size}");
+			printHadamardMatrix(hadamardMatrix);
+		}
+	}
+}
---
+++ Hadamard/Program.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+
+namespace Hadamard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                Console.WriteLine("Please enter a matrix size: ");
+                string size = Console.ReadLine();
+                Hadamard hMatrix = new Hadamard(int.Parse(size));
+                hMatrix.Run(hMatrix.HadamardMatrix);
+
+                Console.Write("Enter \'y\' to continue: ");
+                string go = Console.ReadLine();
+                if (go.ToLower() == "y")
+                    keepGoing = true;
+                else keepGoing = false;
+            }
+        }
+    }
+}
